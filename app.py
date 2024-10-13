8ef7e6b5c27240c7cf4ea6c522ea149dc195df15
Anton Vigovskij <aavigovskij@gmail.com>
2024-10-13 19:08:44 +0300

    Update: added registration

--- app.py
+++ app.py
@@ -5,7 +5,10 @@ import streamlit_authenticator as stauth
 
 import yaml
 from yaml.loader import SafeLoader
-with open('./users_config.yml') as file:
+
+USERS_DB_PATH = './users_config.yml'
+
+with open(USERS_DB_PATH) as file:
     config = yaml.load(file, Loader=SafeLoader)
 
 
@@ -19,14 +22,52 @@ authenticator = stauth.Authenticate(
     config['cookie']['expiry_days']
 )
 
-try:
-    authenticator.login()
-except stauth.LoginError as e:
-    st.error("There is error during login process")
+def login():
+    try:
+        authenticator.login()
+    except stauth.LoginError as e:
+        st.error("There is error during login process")
 
+
+def register():
+    status = None
+    try:
+        email_of_registered_user, \
+        username_of_registered_user, \
+        name_of_registered_user = authenticator.register_user()
+        print('dir(authenticator)')
+        print(dir(authenticator))
+        if email_of_registered_user and username_of_registered_user and name_of_registered_user:
+            with open(USERS_DB_PATH, 'w') as file:
+                first_name, last_name = name_of_registered_user.split(' ')
+
+                config['credentials']['usernames'].update({
+                    username_of_registered_user: {
+                        'email': email_of_registered_user,
+                        'first_name': first_name,
+                        'last_name': last_name,
+                        'roles': ['admin', 'editor', 'viewer'],
+                        'password': 'TestPassword5$',
+                    }
+                })
+                yaml.dump(config, file, default_flow_style=False)
+            status = True
+        else:
+            status = False
+    except stauth.RegisterError as e:
+        st.error(f"There is error during register process: {e}")
+    else:
+        if status:
+            st.success('Successfully registered! Try to log in!')
 
 if st.session_state['authentication_status']:
     authenticator.logout()
     main()
 elif st.session_state['authentication_status'] is False:
-    st.error('Wrong account!')
+    login()
+    st.error('Wrong account! Try to register!')
+    register()
+elif st.session_state['authentication_status'] is None:
+    login()
+    st.warning('Please enter your username and password! Or Register:')
+    register()
