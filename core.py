ec655353f5d2d37efb3278ac8db75fc52249b7d2
Alex <alex@MacBook-Pro-Arsenteva.local>
2024-10-13 20:08:15 +0300

    update front

--- core.py
+++ core.py
@@ -1,118 +1,64 @@
 import pickle
-import surprise
 import pandas as pd
-from sklearn.metrics.pairwise import cosine_similarity
+import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 from PIL import Image
-import numpy as np
 import streamlit as st
+
+# Инициализация списка желаемого и количества отображаемых товаров
+if 'wishlist' not in st.session_state:
+    st.session_state['wishlist'] = []
+
+if 'shown_count' not in st.session_state:
+    st.session_state['shown_count'] = 20  # Количество товаров, которые показываются сразу
 
 
 def main():
     # Header
-    header_image = Image.open('Images/h&mBanner.jpeg')
-    st.image(header_image)
+    # header_image = Image.open('Images/h&mBanner.jpeg')
+    # st.image(header_image)
 
-    # Creating sidebar comments
-    st.sidebar.title('H&M article Recommendations')
+    # Sidebar
+    st.sidebar.title('H&M Article Recommendations')
 
-    # Load in appropriate DataFrames, user ratings
+    # Загрузка данных
     articles_df = pd.read_csv('Data/articles.csv.zip', index_col='article_id')
     articles_df2 = pd.read_csv('Data/articles.csv.zip')
-
-    # Customer data for collabortive filtering
-    df_customer = pd.read_csv('Data/df_customer.csv', index_col='customer_id')
-
-    # Meta data for collabortive filtering
-    transactions = pd.read_csv('Data/out.zip')
-
-    # Meta data for content based
     meta_data = pd.read_csv('Data/out_content.zip')
 
-    # Import final collab model
+    # Загруженные модели
     collab_model = pickle.load(open('Model/collaborative_model.sav', 'rb'))
-    # st.write(articles_df['article_id'] == 893059004)
 
+    # Функция для добавления товаров в список желаемого
+    def add_to_wishlist(article_id):
+        if article_id not in st.session_state['wishlist']:
+            st.session_state['wishlist'].append(article_id)
 
-    # Def function using model to return recommendations - collaborative filtering
-    def customer_article_recommend(customer,n_recs):
-        
-        have_bought = df_customer.loc[customer, 'article_id']
-        not_bought = articles_df.copy()
-        # [not_bought.drop(x, inplace=True) for x in have_bought]
-        not_bought.drop(have_bought, inplace=True)
-        not_bought.reset_index(inplace=True)
-        not_bought['est_purchase'] = not_bought['article_id'].apply(lambda x: collab_model.predict(customer, x).est)
-        not_bought.sort_values(by='est_purchase', ascending=False, inplace=True)
-        
-        not_bought.rename(columns={'prod_name':'Product Name', 'author':'Author',
-                                'product_type_name':'Product Type Name', 'product_group_name':'Product Group Name',
-                                'index_group_name':'Index Group Name', 'garment_group_name ':'Garment Group Name'}, inplace=True)
-        not_bought = not_bought.iloc[:100, :]
-        not_bought.drop(['product_code', 'product_type_no', 'graphical_appearance_no','graphical_appearance_name', 'colour_group_code', 'colour_group_name',
-        'perceived_colour_value_id', 'perceived_colour_value_name','perceived_colour_master_id', 'perceived_colour_master_name',
-        'department_no', 'department_name', 'index_code', 'index_name','index_group_no', 'section_no', 'section_name',
-        'garment_group_no', 'detail_desc','est_purchase'], axis=1, inplace=True)
-        # not_bought = not_bought[['article_id','Product Name', 'Product Type Name', 'Product Group Name', 'Index Group Name', 'Garment Group Name']]
-        not_bought = not_bought.sample(frac=1).reset_index(drop=True)
-        
-        return not_bought.head(n_recs)
+    # Функция для удаления товара из списка желаемого
+    def remove_from_wishlist(article_id):
+        if article_id in st.session_state['wishlist']:
+            st.session_state['wishlist'].remove(article_id)
 
-    # Second function for content based recommendations
-    def article_recommend(article_input, n_recs2):
-        # st.write(article_input[0][0])
-        article = articles_df2[articles_df2['article_id'] == article_input].index
-        y = np.array(meta_data.loc[article]).reshape(1, -1)
-        # st.write(article)
-        cos_sim = cosine_similarity(meta_data, y)
-        cos_sim = pd.DataFrame(data=cos_sim, index=meta_data.index)
-        cos_sim.sort_values(by = 0, ascending = False, inplace=True)
-        results = cos_sim.index.values
-        # results = cos_sim.index.values[1:n_recs2+1]
-        results_df = articles_df2.loc[results]
-        results_df.reset_index(inplace=True)
-        results_df.rename(columns={'prod_name':'Product Name', 'author':'Author',
-                                'product_type_name':'Product Type Name', 'product_group_name':'Product Group Name',
-                                'index_group_name':'Index Group Name', 'garment_group_name ':'Garment Group Name'}, inplace=True)
-        results_df = results_df.iloc[:100, :]
-        results_df.drop(['product_code', 'product_type_no', 'graphical_appearance_no','graphical_appearance_name', 'colour_group_code', 'colour_group_name',
-        'perceived_colour_value_id', 'perceived_colour_value_name','perceived_colour_master_id', 'perceived_colour_master_name',
-        'department_no', 'department_name', 'index_code', 'index_name','index_group_no', 'section_no', 'section_name',
-        'garment_group_no', 'detail_desc', 'index'], axis=1, inplace=True)
-        
-        results_df = results_df.sample(frac=1).reset_index(drop=True)
-        
-        
-        return results_df.head(n_recs2)
+    # Функция для формирования рекомендаций
+    def generate_recommendations_from_wishlist(n_recs):
+        if not st.session_state['wishlist']:
+            st.write("Ваш список желаемого пуст.")
+            return None
+        recommendations = []
+        for article_id in st.session_state['wishlist']:
+            article_recommendations = articles_df2.sample(n=n_recs)  # Пример, можно заменить на содержательный алгоритм
+            recommendations.append(article_recommendations)
+        recommendations_df = pd.concat(recommendations)
+        return recommendations_df
 
-    # print the image of the articles recommended
-    def print_image_cf(results_cf, n_recs):
-        f, ax = plt.subplots(1, n_recs, figsize=(100,50))
-        i = 0
-        article_id_cf = results_cf['article_id']
-        for index, data in enumerate(article_id_cf):
-            desc = articles_df2[articles_df2['article_id'] == data]['detail_desc'].iloc[0]
-            desc_list = desc.split(' ')
-            for j, elem in enumerate(desc_list):
-                if j > 0 and j % 5 == 0:
-                    desc_list[j] = desc_list[j] + '\n'
-            desc = ' '.join(desc_list)
-            img = mpimg.imread(f'Data/h-and-m-personalized-fashion-recommendations/images/0{str(data)[:2]}/0{int(data)}.jpg')
-            ax[i].imshow(img)
-            ax[i].set_xticks([], [])
-            ax[i].set_yticks([], [])
-            ax[i].grid(False)
-            ax[i].set_xlabel(desc, fontsize=90)
-            i += 1
-        return plt.show()
+    # Функция для отображения изображений (по 5 штук в строке)
+    def print_image_cf(results_cf):
+        images_per_row = 5
+        total = len(results_cf)
+        rows = (total // images_per_row) + int(total % images_per_row > 0)
 
-    # print the image of the articles recommended
-    def print_image_cf(results_cf, n_recs):
-        images_per_row = 2
-        rows = (n_recs // images_per_row) + int(n_recs % images_per_row > 0)
-
-        fig, axarr = plt.subplots(rows, images_per_row, figsize=(20, 10 * rows))  # Создаем фигуру
+        fig, axarr = plt.subplots(rows, images_per_row, figsize=(20, 10 * rows))
         article_id_cf = results_cf['article_id']
 
         if rows == 1:
@@ -135,52 +81,82 @@ def main():
             axarr[row][col].set_xticks([])
             axarr[row][col].set_yticks([])
             axarr[row][col].grid(False)
-            axarr[row][col].set_xlabel(desc, fontsize=14)
+            # axarr[row][col].set_xlabel(desc, fontsize=14)
             i += 1
 
         for j in range(i, rows * images_per_row):
             fig.delaxes(axarr[j // images_per_row][j % images_per_row])
 
-        # Передаем объект fig в st.pyplot
         st.pyplot(fig)
 
-    st.sidebar.subheader('This recommendation system can make two forms of recommendations.')
-    st.sidebar.write('Existing customers looking for articles they might like.')
-    st.sidebar.write('Similiar articles based on article input by customer.')
+    # Страница списка желаемого с возможностью удаления товаров и отрисовки картинок
+    def wishlist_page():
+        st.title("Ваш список желаемого")
+        if not st.session_state['wishlist']:
+            st.write("Ваш список желаемого пуст.")
+            return
 
-    st.title('H&M Recommender System')
-    st.subheader('This app is a article recommender system for H&M')
-    st.subheader("See the sidebar navigation for options")
+        images_per_row = 5
+        for i, article_id in enumerate(st.session_state['wishlist']):
+            # Получаем информацию о статье
+            article_info = articles_df2[articles_df2['article_id'] == article_id].iloc[0]
+            img_path = f'Data/h-and-m-personalized-fashion-recommendations/images/0{str(article_id)[:2]}/0{int(article_id)}.jpg'
 
-    page_names = ['Existing Customer', 'Similar Article']
-    page = st.sidebar.radio('Navigation', page_names)
+            # Отображаем картинку и информацию о товаре
+            cols = st.columns([1, 4])
+            with cols[0]:
+                st.image(img_path, width=150)
+            with cols[1]:
+                st.write(f"Товар ID: {article_id} - {article_info['prod_name']}")
+                if st.button(f"Удалить {article_id}", key=f"remove_{article_id}"):
+                    remove_from_wishlist(article_id)
 
-    st.sidebar.caption('Please refer to my [Github](https://github.com/aliceagrawal/HM-Recommender-System-App) for reference to the code.')
+    # Главная страница с товарами и выбором тем
+    def main_page():
+        st.subheader("Выберите товары для добавления в список желаемого:")
 
+        # Выбор тем (категорий)
+        categories = articles_df2['product_type_name'].unique()
+        st.sidebar.subheader('Выберите интересные вам категории')
+        selected_categories = st.sidebar.multiselect('Категории', categories)
 
+        # Фильтрация товаров по выбранным категориям
+        if selected_categories:
+            filtered_articles = articles_df2[articles_df2['product_type_name'].isin(selected_categories)]
+        else:
+            filtered_articles = articles_df2.copy()
 
-    if page == 'Existing Customer':
-        st.header("You chose the existing customer option.")
-        customer_input = st.text_input("Please input your unique Customer ID.")
-        n_recs = st.number_input("Please enter the number of article recommendations you would like.", max_value=20)
-        rec_button = st.button("Get some recommendations...")
-        if rec_button:
-            results = customer_article_recommend(customer_input, n_recs)
-            st.table(results)
-            result_image = print_image_cf(results, n_recs)
-            # st.set_option('deprecation.showPyplotGlobalUse', False)
-            st.pyplot(result_image)
+        total_items = len(filtered_articles)
 
+        # Отображение товаров
+        for index, row in filtered_articles.iterrows():
+            if index >= st.session_state['shown_count']:
+                break
+            st.image(
+                f'Data/h-and-m-personalized-fashion-recommendations/images/0{str(row["article_id"])[:2]}/0{int(row["article_id"])}.jpg',
+                width=150)
+            st.write(row['prod_name'])
+            if st.button(f"Добавить {row['article_id']} в список желаемого", key=f"wishlist_{index}"):
+                add_to_wishlist(row['article_id'])
 
-    else:
-        st.header("You chose the similar articles option.")
-        article_input = st.number_input("Please enter a article ID.", max_value=959461001)
-        # article = articles_df2.index[articles_df2['article_id'] == article_input]
-        n_recs2 = st.number_input("Please enter the number of recommendations you would like.", max_value=20, key=2)
-        book_button = st.button("Get some recommendations...", key=2)
-        if book_button:
-            results2 = article_recommend(article_input, n_recs2)
-            st.table(results2)
-            result_image2 = print_image_cf(results2, n_recs2)
-            # st.set_option('deprecation.showPyplotGlobalUse', False)
-            st.pyplot(result_image2)
+        # Кнопка "Показать еще"
+        if st.session_state['shown_count'] < total_items:
+            if st.button("Показать еще"):
+                st.session_state['shown_count'] += 20
+
+        # Кнопка рекомендаций
+        if st.button("Получить рекомендации"):
+            recommendations = generate_recommendations_from_wishlist(5)
+            if recommendations is not None:
+                st.subheader("Рекомендации:")
+                st.table(recommendations[['prod_name', 'product_type_name', 'product_group_name']])
+                result_image = print_image_cf(recommendations)
+
+    # Sidebar Navigation
+    page_names = ['Главная страница', 'Список желаемого']
+    page = st.sidebar.radio('Навигация', page_names)
+
+    if page == 'Главная страница':
+        main_page()
+    elif page == 'Список желаемого':
+        wishlist_page()
